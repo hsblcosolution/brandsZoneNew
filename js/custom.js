e52f0ec92804925a3e42220b799e2d79025d5ab9
emon1432 <ovirajemon143@gmail.com>
2022-05-14 15:01:02 +0600

    create best selling

--- js/custom.js
+++ js/custom.js
@@ -24,31 +24,70 @@ $(document).ready(function() {
         }
     })
 
+    //best selling add
+    $(".best_selling_add").owlCarousel({
+        nav: true,
+        dots: false,
+        loop: false,
+        row: 2,
+
+        rewind: true,
+        // autoWidth: true,
+        stagePadding: 0,
+        margin: 4,
+        checkVisibility: true,
+        navElement: 'div',
+        responsive: {
+            0: {
+                items: 1,
+                slideBy: 1
+            },
+            768: {
+                items: 2,
+                slideBy: 2
+            },
+            1024: {
+                items: 6,
+                slideBy: 3
+            },
+            1280: {
+                items: 6,
+                slideBy: 3
+            },
+            1440: {
+                items: 6,
+                slideBy: 1
+            }
+        }
+    });
+
     //trending slider
     $('.trending_slider_add').owlCarousel({
-        loop:true,
-        margin:20,
-        nav:true,
-        navText:['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+        loop: true,
+        margin: 30,
+        nav: true,
+        navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
         animateIn: 'fadeIn',
-        autoplay:true,
-        autoplayTimeout:2000,
+        autoplay: true,
+        autoplayTimeout: 2000,
         responsiveClass: true,
         responsive: {
             0: {
                 items: 2,
-               
+
             },
             600: {
                 items: 3,
-                
+
             },
             1000: {
-                items:6,
-               
-               
+                items: 6,
+
+
             }
         }
     })
+
+
 
 });
